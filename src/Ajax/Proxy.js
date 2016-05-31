0c43ffa13de813ddf9419360d4723c38962ac2c9
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-06-01 01:54:58 +0200

    Fix error loading

--- src/Ajax/Proxy.js
+++ src/Ajax/Proxy.js
@@ -1,25 +1,25 @@
 GollumJS.NS(GollumJS.Ajax, function() {
-
+	
 	var Promise = GollumJS.Promise;
 	var JSON = JSON3;
-
+	
 	this.Proxy = new GollumJS.Class({
-
+		
 		Extends: GollumJS.Ajax,
-
+		
 		calling: {},
 		results: {},
-
+		
 		initialize: function () {
 			this.parent().apply(this, arguments);
 		},
-
-
+		
+		
 		request: function (param) {
-
+			
 			var _this = this;
 			var id = this.getId(param);
-
+			
 			if (this.results[id] !== undefined) {
 				return Promise.resolve(this.results[id]);
 			}
@@ -31,37 +31,40 @@ GollumJS.NS(GollumJS.Ajax, function() {
 					});
 				});
 			}
-
+			
 			this.calling[id] = [];
-
+			
 			return new Promise(function (resolve, reject) {
-
+				
 				_this.calling[id].push({
 					resolve: resolve, 
 					reject : reject
 				});
-
+				
 				_this.parent().request(param)
 					.then(function (data) {
 						
 						_this.results[id] = data;
-
-						for (var i = 0; i < _this.calling[id].length; i++) {
-							_this.calling[id][i].resolve(data);
+						
+						if (_this.calling[id]) {
+							for (var i = 0; i < _this.calling[id].length; i++) {
+								_this.calling[id][i].resolve(data);
+							}
+							delete (_this.calling[id]);
 						}
-						delete (_this.calling[id]);
 					})
 					.catch(function (error) {
 						if (_this.calling[id]) {
 							for (var i = 0; i < _this.calling[id].length; i++) {
 								_this.calling[id][i].reject(error);
 							}
+							delete _this.calling[id];
 						}
 					})
 				;
 			});
 		},
-
+		
 		getId: function (param) {
 			return JSON.stringify ([
 				param.url,
